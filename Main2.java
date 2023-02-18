8fb5b7229a493716875e4b719f804a4e8eafc209
Kadehis <124196897+Kadehis@users.noreply.github.com>
2023-02-18 18:51:56 +0100

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        int n = 365;
+        int e = n % 10;
+        int d = n / 10 % 10;
+        int s = n / 100;
+
+        System.out.println( s);
+        System.out.println( d);
+        System.out.println(e);
+          }
+}
